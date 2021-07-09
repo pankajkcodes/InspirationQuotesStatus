9d5b3398f908f9cbd067c5537b5fb49ae35a9727
pankajkcodes <pankajkcodes@gmail.com>
2021-07-09 08:52:28 +0530

    commit after json file updated

--- app/src/main/java/com/pankajkcodes/inspirationquotesstatus/Adapter.java
+++ app/src/main/java/com/pankajkcodes/inspirationquotesstatus/Adapter.java
@@ -1,10 +1,16 @@
 package com.pankajkcodes.inspirationquotesstatus;
 
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,11 +19,13 @@ import java.util.ArrayList;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
     Context context;
-    ArrayList<QuotesData> list;
+    ArrayList<String> quotes ;
+    ArrayList<String> author ;
 
-    public Adapter(Context context, ArrayList<QuotesData> list) {
+    public Adapter(Context context, ArrayList<String> quotes, ArrayList<String> author) {
         this.context = context;
-        this.list = list;
+        this.quotes = quotes;
+        this.author = author;
     }
 
     @NonNull
@@ -27,28 +35,47 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
         return new ViewHolder(view);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        QuotesData data = list.get(position);
-        holder.text.setText(data.getText());
-        holder.author.setText("Author : "+data.getAuthor());
+        holder.text.setText(quotes.get(position));
+        holder.author.setText("Author : "+author.get(position));
+        holder.copyBtn.setOnClickListener((v)->{
+            ClipboardManager clipboardManager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clipData = ClipData.newPlainText("text",holder.text.getText()+"\n"+
+                    holder.author.getText());
+            if (clipboardManager != null){
+                clipboardManager.setPrimaryClip(clipData);
+                Toast.makeText(context, "Copied", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.shareBtn.setOnClickListener((v)->{
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT,holder.text.getText()+"\n"+
+                    holder.author.getText());
+            context.startActivity(Intent.createChooser(intent,"Share to"));
+        });
 
     }
 
     @Override
     public int getItemCount() {
-        return list.size();
+        return quotes.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView text;
         TextView author;
+        Button shareBtn,copyBtn;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             text = itemView.findViewById(R.id.quotes);
             author = itemView.findViewById(R.id.author);
+            shareBtn = itemView.findViewById(R.id.shareBtn);
+            copyBtn = itemView.findViewById(R.id.copyBtn);
         }
     }
 }
--- app/src/main/java/com/pankajkcodes/inspirationquotesstatus/MainActivity.java
+++ app/src/main/java/com/pankajkcodes/inspirationquotesstatus/MainActivity.java
@@ -1,98 +1,94 @@
 package com.pankajkcodes.inspirationquotesstatus;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
-    private String text,author;
 
-    ApiInterfaceEndPoint apiInterface;
+    ArrayList<String> quotes = new ArrayList<>();
+    ArrayList<String> author = new ArrayList<>();
 
-    String url = "";
+    String url = "https://play.google.com/store/apps/details?id=com.simplifiededtech.allhindishayariandquotes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initDataApi();
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        try {
+            JSONObject jsonObject = new JSONObject(Objects.requireNonNull(JsonDataFromAssets()));
+            JSONArray jsonArray = jsonObject.getJSONArray("quotes");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject quotesData = jsonArray.getJSONObject(i);
+                quotes.add(quotesData.getString("text"));
+                author.add(quotesData.getString("author"));
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Adapter adapter = new Adapter(MainActivity.this, quotes, author);
+        recyclerView.setAdapter(adapter);
 
     }
 
-    private void initDataApi() {
-        apiInterface = RetrofitInstance.getRetrofit().create(ApiInterfaceEndPoint.class);
+    private String JsonDataFromAssets() {
+        String json;
+        try {
+            InputStream inputStream = getAssets().open("quotes.json");
+            int sizeOfFile = inputStream.available();
+            byte[] bufferData = new byte[sizeOfFile];
+            inputStream.read(bufferData);
+            inputStream.close();
+            json = new String(bufferData, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        return json;
 
-        apiInterface.getQuotes().enqueue(new Callback<List<QuotesData>>() {
-            @Override
-            public void onResponse(Call<List<QuotesData>> call, Response<List<QuotesData>> response) {
-                if (response.body().size()>0){
-                    ArrayList<QuotesData> list = (ArrayList<QuotesData>) response.body();
-                    // Initialize first recycler view
-                    recyclerView = findViewById(R.id.recyclerView);
-                    list.add(new QuotesData());
-                    Adapter adapter = new Adapter(MainActivity.this,list);
-                    recyclerView.setAdapter(adapter);
-                    LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
-                    recyclerView.setLayoutManager(layoutManager);
-
-                }else {
-                    Toast.makeText(MainActivity.this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<QuotesData>> call, Throwable t) {
-                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
-            Log.d("error",t.getMessage());
-            }
-        });
     }
+
 
     @Override
     public void onBackPressed() {
         //--------> Create a dialog for Show dialog after click backed --->//
-        AlertDialog.Builder builder =  new AlertDialog.Builder(this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Are you sure you want to exit ?")
                 .setCancelable(false)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        MainActivity.super.onBackPressed();
-                    }
-                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        }).setNeutralButton("More Apps", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
+                .setPositiveButton("Yes", (dialog, which) -> MainActivity.super.onBackPressed()).setNegativeButton("No", (dialog, which) -> dialog.cancel()).setNeutralButton("More Apps", (dialog, which) -> {
 
-                //--------> Code for More Apps --->//
-                try {
-                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-                }catch (Exception e){
-                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+                    //--------> Code for More Apps --->//
+                    try {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                    } catch (Exception e) {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
         builder.create();
         builder.show();
     }
